b8b78188514fd6acb637a6daeaecfba3014a49d9
richen604 <56615615+richen604@users.noreply.github.com>
2024-11-04 22:19:20 -0800

    ci: setup commit-lint & changelog ci

---
+++ .github/pull_request_template.md
@@ -0,0 +1,17 @@
+## Description
+<!-- Describe your changes in detail -->
+
+## Type of change
+<!-- Please delete options that are not relevant -->
+
+- [ ] Bug fix (non-breaking change which fixes an issue)
+- [ ] New feature (non-breaking change which adds functionality)
+- [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
+- [ ] Documentation update
+
+## Checklist
+<!-- Please check if your PR fulfills these requirements -->
+
+- [ ] My commits follow conventional commit format
+- [ ] I have updated the documentation accordingly
+- [ ] My changes generate no new warnings 
--- flake.nix
+++ flake.nix
@@ -48,6 +48,32 @@
       };
       archVMConfig = import ./hosts/vm/arch-vm.nix;
       fedoraVMConfig = import ./hosts/vm/fedora-vm.nix;
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          commitlint
+          pnpm
+          direnv
+          nix-direnv
+        ];
+
+        shellHook = ''
+          export USE_DIRENV=1
+
+          # Install dependencies and setup husky if needed
+          if [ ! -d "node_modules" ]; then
+            pnpm install
+            pnpm prepare
+          fi
+
+          # Set up commit-msg hook if it doesn't exist
+          mkdir -p .husky
+          echo '#!/usr/bin/env sh
+          pnpm dlx commitlint --edit "$1"' > .husky/commit-msg
+          chmod +x .husky/commit-msg
+        '';
+      };
     in
     {
       nixosConfigurations = {
@@ -118,5 +144,7 @@
         };
         activationPackage = self.homeConfigurations.${userConfig.username}.activationPackage;
       };
+
+      devShells.${system}.default = devShell;
     };
 }
