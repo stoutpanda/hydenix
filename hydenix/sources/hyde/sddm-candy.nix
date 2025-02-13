00d6952fdd68def82489422d788cc64979695dd4
richen604 <56615615+richen604@users.noreply.github.com>
2025-02-12 19:50:00 -0800

    feat: create sddm-candy package

--- hydenix/sources/hyde/sddm-candy.nix
+++ hydenix/sources/hyde/sddm-candy.nix
@@ -1,13 +1,13 @@
 {
   pkgs,
+  commit,
 }:
-
 pkgs.stdenv.mkDerivation {
   pname = "sddm-candy-theme";
   version = "1.0.0";
 
   src = pkgs.fetchzip {
-    url = "https://github.com/prasanthrangan/hyprdots/raw/refs/heads/main/Source/arcs/Sddm_Candy.tar.gz";
+    url = "https://github.com/HyDE-Project/HyDE/raw/${commit}/Source/arcs/Sddm_Candy.tar.gz";
     hash = "sha256-H8qmLpTFFaTFJVSKAHQBMacPCWlySagmWLTvNYrv7Ds=";
   };
 
@@ -18,7 +18,7 @@ pkgs.stdenv.mkDerivation {
 
   meta = with pkgs.lib; {
     description = "Candy theme for SDDM";
-    homepage = "https://github.com/prasanthrangan/hyprdots";
+    homepage = "https://github.com/HyDE-Project/HyDE";
     license = licenses.gpl3;
     platforms = platforms.linux;
     maintainers = [ ];
