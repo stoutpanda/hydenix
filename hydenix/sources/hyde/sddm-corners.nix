3bf2f6fb95bd8885a959ff98c8184a807dc13971
richen604 <56615615+richen604@users.noreply.github.com>
2025-02-13 17:22:58 -0800

    feat: created sddm-corners package

--- hydenix/sources/hyde/sddm-corners.nix
+++ hydenix/sources/hyde/sddm-corners.nix
@@ -1,13 +1,13 @@
 {
   pkgs,
+  commit,
 }:
-
 pkgs.stdenv.mkDerivation {
   pname = "sddm-corners-theme";
   version = "1.0.0";
 
   src = pkgs.fetchzip {
-    url = "https://github.com/prasanthrangan/hyprdots/raw/main/Source/arcs/Sddm_Corners.tar.gz";
+    url = "https://github.com/HyDE-Project/HyDE/raw/${commit}/Source/arcs/Sddm_Corners.tar.gz";
     hash = "sha256-vpHsLcGKtlZQHMI2cc76aRycpaYxig2u0zDd3EmbnVk=";
   };
 
@@ -18,7 +18,7 @@ pkgs.stdenv.mkDerivation {
 
   meta = with pkgs.lib; {
     description = "Corners theme for SDDM";
-    homepage = "https://github.com/prasanthrangan/hyprdots";
+    homepage = "https://github.com/HyDE-Project/HyDE";
     license = licenses.gpl3;
     platforms = platforms.linux;
     maintainers = [ ];
