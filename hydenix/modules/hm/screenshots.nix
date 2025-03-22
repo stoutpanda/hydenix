958fbdbb9667d0835dbf86c6d4015ec8ca32f2de
richen604 <56615615+richen604@users.noreply.github.com>
2025-03-21 21:47:22 -0700

    feat: created screenshots module

---
+++ hydenix/modules/hm/screenshots.nix
@@ -0,0 +1,60 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.hydenix.hm.screenshots;
+in
+{
+  options.hydenix.hm.screenshots = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = config.hydenix.hm.enable;
+      description = "Enable screenshots module";
+    };
+
+    grim = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable grim screenshot tool";
+      };
+    };
+
+    slurp = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable slurp region selection tool";
+      };
+    };
+
+    satty = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable satty screenshot annotation tool";
+      };
+    };
+
+    swappy = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable swappy screenshot editor";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      (lib.mkIf cfg.grim.enable grim) # screenshot tool
+      (lib.mkIf cfg.slurp.enable slurp) # region selection tool
+      (lib.mkIf cfg.satty.enable satty) # screenshot annotation tool
+      (lib.mkIf cfg.swappy.enable swappy) # screenshot editor
+    ];
+  };
+}
