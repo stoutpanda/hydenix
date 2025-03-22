bd22f16c191ebcf8c69d06b60ca958fa76bbf23c
richen604 <56615615+richen604@users.noreply.github.com>
2025-03-21 21:48:25 -0700

    feat: created spotify module

---
+++ hydenix/modules/hm/spotify.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+# TODO: add spicetify support using flatpak
+let
+  cfg = config.hydenix.hm.spotify;
+in
+{
+  options.hydenix.hm.spotify = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = config.hydenix.hm.enable;
+      description = "Enable spotify module";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      spotify
+    ];
+
+    home.file = {
+      ".config/spotify-flags.conf" = lib.mkDefault {
+        source = "${pkgs.hydenix.hyde}/Configs/.config/spotify-flags.conf";
+      };
+      ".config/electron-flags.conf" = lib.mkDefault {
+        source = "${pkgs.hydenix.hyde}/Configs/.config/electron-flags.conf";
+      };
+    };
+  };
+}
