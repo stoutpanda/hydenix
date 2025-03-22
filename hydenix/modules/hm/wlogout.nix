35a9e3be478370469d9d4fbfa23a7b61cc6f0bdd
richen604 <56615615+richen604@users.noreply.github.com>
2025-03-21 21:50:38 -0700

    feat: created wlogout module

---
+++ hydenix/modules/hm/wlogout.nix
@@ -0,0 +1,67 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.hydenix.hm.wlogout;
+in
+{
+  options.hydenix.hm.wlogout = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = config.hydenix.hm.enable;
+      description = "Enable logout module";
+    };
+
+    wlogout = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable wlogout";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+
+      # wlogout
+      (lib.mkIf cfg.wlogout.enable wlogout) # logout menu
+    ];
+
+    home.file = {
+
+      # icons
+      ".config/wlogout/icons/" = lib.mkDefault {
+        source = "${pkgs.hydenix.hyde}/Configs/.config/wlogout/icons/";
+        recursive = true;
+      };
+
+      # Stateful files with themes
+      ".config/wlogout/layout_1" = lib.mkDefault {
+        source = "${pkgs.hydenix.hyde}/Configs/.config/wlogout/layout_1";
+        force = true;
+        mutable = true;
+      };
+      ".config/wlogout/style_1.css" = lib.mkDefault {
+        source = "${pkgs.hydenix.hyde}/Configs/.config/wlogout/style_1.css";
+        force = true;
+        mutable = true;
+      };
+      ".config/wlogout/layout_2" = lib.mkDefault {
+        source = "${pkgs.hydenix.hyde}/Configs/.config/wlogout/layout_2";
+        force = true;
+        mutable = true;
+      };
+      ".config/wlogout/style_2.css" = lib.mkDefault {
+        source = "${pkgs.hydenix.hyde}/Configs/.config/wlogout/style_2.css";
+        force = true;
+        mutable = true;
+      };
+
+    };
+  };
+}
