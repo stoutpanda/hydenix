a719a48761c0af9228da794a850c8a6de8017608
richen604 <56615615+richen604@users.noreply.github.com>
2025-06-09 20:23:50 -0700

    feat: good bluetooth default

--- hydenix/modules/system/system.nix
+++ hydenix/modules/system/system.nix
@@ -81,6 +81,12 @@ in
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+        };
+      };
     };
 
     services = {
