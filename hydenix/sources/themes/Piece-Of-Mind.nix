e92cb44493f5894b6cc8b852eb3abe1d29b10a93
richen604 <56615615+richen604@users.noreply.github.com>
2025-06-26 17:59:58 -0700

    fix: bumped theme hash

--- hydenix/sources/themes/Piece-Of-Mind.nix
+++ hydenix/sources/themes/Piece-Of-Mind.nix
@@ -6,7 +6,7 @@ mkTheme rec {
     repo = "Piece-Of-Mind";
     rev = "main";
     name = name;
-    sha256 = "sha256-0rfiJ2CQPsaNRr+C1RUsEKIIpu8Hre11UInlorScqfI=";
+    sha256 = "sha256-TKaA1nFB8w0QMQvUxVhzGGBAMHWCQoutH9gC8sIVTZk=";
   };
   meta = {
     name = name;
